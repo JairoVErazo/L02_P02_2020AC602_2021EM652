e82ce4edd3179508ac2464fefabfb786987df0b1
Jairo Erazo <132089241+JairoVErazo@users.noreply.github.com>
2024-04-20 13:21:21 -0600

    Agregar archivos de proyecto.

---
+++ L02_P02_2020AC602_2021EM652/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02_P02_2020AC602_2021EM652.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
